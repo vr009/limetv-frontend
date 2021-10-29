8d69c516aada9b4ab1f59c695c6cd23546bbe97f
Blackbackofficial <Chernov-Ivan.1997@yandex.ru>
2021-10-29 15:54:37 +0300

    Перенос папки

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 import {createMenu} from './components/menu/menu.js';
 import {createFilms} from './components/films/films.js';
 
+const root = document.getElementById('root');
+const menu = document.createElement('div');
+menu.setAttribute('id', 'menu');
+root.innerHTML = '';
+root.appendChild(menu);
 createMenu();
 createFilms();
