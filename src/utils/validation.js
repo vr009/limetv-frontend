b1b6d766c54e92d9dacf08e3a93083a602ebc26a
vr009 <slavarianov@yandex.ru>
2021-10-29 16:00:04 +0300

    validation for routing added

---
+++ src/utils/validation.js
@@ -0,0 +1,53 @@
+const regExpressions = {
+  username: /^[a-zA-Z0-9_]{3,}$/,
+  // ASCII chars from ! to ~
+  password: /[!-~]{6,}/,
+  userLink: /^\/user\/[\w-.]{1,}$/,
+  loginLink: /^\/auth\/login\//,
+  signupLink: /^\/auth\/signup\//,
+  filmLink: /^\/film\/[\w-.]{1,}$/,
+  actorLink: /^\/actor\/[\w-.]{1,}$/,
+  playerLink: /^\/player\/[\w-.]{1,}$/,
+};
+
+export const validators = {
+  username: (username) => validateField(username, regExpressions.username),
+  password: (password) => validateField(password, regExpressions.password),
+  userLink: (userLink) => validateField(userLink, regExpressions.userLink),
+  loginLink: (loginLink) => validateField(loginLink, regExpressions.pinsUserLink),
+  signupLink: (signupLink) => validateField(signupLink, regExpressions.deskUserLink),
+  filmLink: (filmLink) => validateField(filmLink, regExpressions.chatUserLink),
+  actorLink: (actorLink) => validateField(actorLink, regExpressions.chatUserLink),
+  playerLink: (playerLink) => validateField(playerLink, regExpressions.chatUserLink),
+};
+
+const validateField = (field, regExp) => {
+  return regExp.test(String(field));
+};
+
+/**
+ * validateCreateBoard
+ * проверка вводимых данных
+ * @param {string} name - имя пина
+ * @param {string} description - описание пина
+ * @return {boolean} isOk?
+ */
+export function validateCreateBoard(name, description) {
+
+  if (name.length < 5 || name.length > 65 )
+    return false;
+
+  // @todo do we need description check?
+  // it can be 0 length
+  return true;
+}
+
+/**
+ * validateAddPinComment
+ * проверка вводимых данных
+ * @param {string} comment - комментарий
+ * @return {boolean} isOk?
+ */
+export function validateAddPinComment(comment) {
+  return !(comment.length < 3 || comment.length > 64);
+}
