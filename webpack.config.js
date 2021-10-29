cf8ef0bd1d97f1f4784fb80b81a3b7ac038d8452
vr009 <slavarianov@yandex.ru>
2021-10-30 01:08:32 +0300

    basic webpack conf ready

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,48 @@
+const HTMLWebPackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
-    path: __dirname,
+    path: __dirname + '/dist/',
     filename: 'bundle.js',
   },
   target: 'node',
+  plugins: [
+    new HTMLWebPackPlugin({
+      template: './src/view/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [{
+      test: /\.pug$/,
+      oneOf: [
+        // this applies to pug imports inside JavaScript
+        {
+          use: ['pug-loader'],
+        },
+      ],
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/',
+    }, {
+      test: /\.(png|jpg|gif|svg)$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]',
+      },
+    },{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true },
+        },
+      ],
+    }],
+  },
 };
