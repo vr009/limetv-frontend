dc253cb8b5ef03e265748b4f5d0d758b645dd36f
vr009 <slavarianov@yandex.ru>
2021-12-01 00:55:03 +0300

    watch btn margin changed

--- src/utils/locale.js
+++ src/utils/locale.js
@@ -1,2 +1,2 @@
-export const serverLocate = 'http://3.67.182.34:8000';
-// export const serverLocate = 'http://localhost:8000';
+// export const serverLocate = 'http://3.67.182.34:8000';
+export const serverLocate = 'http://localhost:8000/api';
