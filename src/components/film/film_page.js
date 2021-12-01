d32fcb1febe1256669d836a0c3c049f0dfba24f8
Blackbackofficial <Chernov-Ivan.1997@yandex.ru>
2021-12-01 17:58:23 +0300

    Правка плеера

--- src/components/film/film_page.js
+++ src/components/film/film_page.js
@@ -7,6 +7,7 @@ import {serverLocate} from '../../utils/locale.js';
 import Router from '../../utils/router.js';
 import {getMonth, getTimeFromMins, sklonenieSeries} from '../utils/validate.js';
 import {fetchRequest} from '../network/fetch.js';
+import {createMenu} from "../menu/menu";
 
 /**
  * Модуль создания страницы фильма
@@ -17,6 +18,9 @@ export const createFilmPage = (id) => {
 };
 
 const createBase = (id) => {
+  if (document.getElementById('navbar') === null) {
+    createMenu();
+  }
   if (document.getElementById('stuff') === null) {
     const root = document.getElementById('root');
     root.innerHTML = '';
