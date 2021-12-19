32a36efa2627e276ce7c26e79e32d5cd16cb1cab
vr009 <slavarianov@yandex.ru>
2021-12-20 00:29:54 +0300

    functionality of player

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -229,17 +229,13 @@ export const createPlayerPage = (src, title, pics, num) => {
   const prev = document.querySelector('.player-prev__btn');
   prev.addEventListener('click', function(event) {
     event.preventDefault();
-    rootPage.innerHTML = PlayerPug({
-      videoSrc: pics[(num-1)%pics.length],
-    });
+    Router.go('/player/'+pics[(num+1)%pics.length], title, pics);
     // document.onkeydown = null;
   });
   const next = document.querySelector('.player-next__btn');
   next.addEventListener('click', function(event) {
     event.preventDefault();
-    rootPage.innerHTML = PlayerPug({
-      videoSrc: pics[(num-1)%pics.length],
-    });
+    Router.go('/player/'+pics[(num-1)%pics.length], title, pics);
     // document.onkeydown = null;
   });
   const close = document.getElementById('player-close');
