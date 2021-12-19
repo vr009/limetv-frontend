b2f6cf783e9ba596f078782c2bfb3e07c83c13f1
vr009 <slavarianov@yandex.ru>
2021-12-20 01:19:51 +0300

    functionality of player

--- src/utils/router.js
+++ src/utils/router.js
@@ -94,9 +94,7 @@ export class Router {
         if (state !== null) {
           url += '/watch';
         }
-        if (state.current !== null) {
-          url += '-' + (state.current + 1);
-        }
+
         window.history.replaceState(
             state, // объект состояния
             state.title, // заголовок состояния
